be5754c326d495fb96292ff62c2b85ee7c8e107a
Paul Reioux <reioux@gmail.com>
2020-08-19 17:11:20 -0700

    mm: Enhance per process reclaim to consider shared pages
    Some pages could be shared by several processes. (ex, libc)
    In case of that, it's too bad to reclaim them from the beginnig.
    
    This patch causes VM to keep them on memory until last task
    try to reclaim them so shared pages will be reclaimed only if
    all of task has gone swapping out.
    
    This feature doesn't handle non-linear mapping on ramfs because
    it's very time-consuming and doesn't make sure of reclaiming and
    not common.
    
    Change-Id: I7e5f34f2e947f5db6d405867fe2ad34863ca40f7
    Signed-off-by: Sangseok Lee <sangseok.lee@lge.com>
    Signed-off-by: Minchan Kim <minchan@kernel.org>
    Patch-mainline: linux-mm @ 9 May 2013 16:21:27
    [vinmenon@codeaurora.org: trivial merge conflict fixes + changes
    to make the patch work with 3.18 kernel]
    Signed-off-by: Vinayak Menon <vinmenon@codeaurora.org>
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>

--- release/src-rt-5.02hnd/kernel/linux-4.1/mm/vmscan.c
+++ release/src-rt-5.02hnd/kernel/linux-4.1/mm/vmscan.c
@@ -104,6 +104,13 @@ struct scan_control {
 
 	/* Number of pages freed so far during a call to shrink_zones() */
 	unsigned long nr_reclaimed;
+
+	/*
+	 * Reclaim pages from a vma. If the page is shared by other tasks
+	 * it is zapped from a vma without reclaim so it ends up remaining
+	 * on memory until last task zap it.
+	 */
+	struct vm_area_struct *target_vma;
 };
 
 #define lru_to_page(_head) (list_entry((_head)->prev, struct page, lru))
@@ -1020,7 +1027,7 @@ static unsigned long shrink_page_list(struct list_head *page_list,
 		 * processes. Try to unmap it here.
 		 */
 		if (page_mapped(page) && mapping) {
-			switch (try_to_unmap(page, ttu_flags)) {
+			switch (try_to_unmap(page, ttu_flags, sc->target_vma)) {
 			case SWAP_FAIL:
 				goto activate_locked;
 			case SWAP_AGAIN:
